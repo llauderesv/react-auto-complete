f8a05b1a7940c83d348ac03de2aaa459d9659c18
Vince Llauderes <llauderesv@gmail.com>
2023-01-15 14:11:39 +0800

    auto complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function useFruits(value) {
+  const [data, setData] = useState(['']);
+
+  useEffect(() => {
+    const fruits = [
+      'strawberry',
+      'blueberry',
+      'berry',
+      'apple',
+      'bananas',
+      'pineapple',
+    ].filter(fruits => fruits.toLowerCase().includes(value.toLowerCase()));
+
+    setData(fruits);
+  }, [value]);
+
+  return data;
+}
+
+function useDebounceValue(value, delay) {
+  const [debounceValue, setDebounceValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebounceValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debounceValue;
+}
+
 function App() {
+  const [text, setText] = useState('');
+  const debounceText = useDebounceValue(text, 3000);
+  const fruits = useFruits(debounceText);
+
+  const handleChange = event => {
+    setText(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>React auto complete</h1>
+      <input type='text' value={text} onChange={handleChange} />
+      <pre>Search Term: {debounceText || 'No search'}</pre>
+      <pre>{JSON.stringify(fruits)}</pre>
     </div>
   );
 }
